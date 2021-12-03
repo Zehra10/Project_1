1761c8ee65e839e0975a6c2d3c46ccb027fb3d7c
Zehra Keşkek <fzk1800@gmail.com>
2021-12-03 21:49:10 +0300

    iki fonksyon tamamlandı fakat ilkinde hala sorun var

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
 const body = document.querySelector("body"); //document.querySelector_dosyadan seç
 const button = document.querySelector("button");
-const colors =["black", "green" , "grey", "blue"];
+const colors =["#FFA500", "#FF9C00" , "#FF7F50", "#FF6347", "#FF4500"];
 
 button.addEventListener("click", changeBackground);//butona her tıkladığımda changeBackground fonksyonunu çaliştır
+//addEventListener_kullanıcı sayfada herhangi bir işlem yaptığında(click vb.) istediğimiz aksiyonları alabilmemizi sağlar.
+
+
+
 
 let sıra = 0;
 
 function changeBackground() {
 
     //RASTGELE BİR RENK
-
-   /* const rastgeleSayi = Math.floor(Math.random() * colors.lenght);// Math.random rastgele sayı seçer_ Math.floor(Math.random() rastgele tam sayı seçer
+/*
+    const rastgeleSayi = Math.floor(Math.random() * colors.lenght);// Math.random rastgele sayı seçer_ Math.floor(Math.random() rastgele tam sayı seçer
     const secilenRenk = colors[rastgeleSayi];//rastgele renk seç
-    console.log(rastgeleSayi, secilenRenk);
-    body.style.backgroundColor = secilenRenk;*/
-
+    body.style.backgroundColor = secilenRenk;
+*/
     //sırayla arkaplan rengi seçmek için
 
-    console.log(sıra);
-    if(sıra == 4) sıra = 0;
+   
+    if(sıra == 5) sıra = 0; // sıra dörde eşitlenince 0 dan tekrar başla
     const secilenRenk = colors[sıra];
     sıra++;
     body.style.backgroundColor = secilenRenk;
-
 }
