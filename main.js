58e26ca0e17ba78117664a4fe09784c72883f48e
Zehra Keşkek <fzk1800@gmail.com>
2021-12-03 20:14:23 +0300

    butona tıklandığınde arkaplanı rastgele bir renk yapan ve bir sırayla renk yapan fonksyonlar yazıldı

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-const body = document.querySelector("body");
+const body = document.querySelector("body"); //document.querySelector_dosyadan seç
 const button = document.querySelector("button");
-,const colors =( "black", "green" , "grey", "blue")
+const colors =["black", "green" , "grey", "blue"];
 
-button.addEventListener("click", changeBackground);
+button.addEventListener("click", changeBackground);//butona her tıkladığımda changeBackground fonksyonunu çaliştır
 
+let sıra = 0;
 
 function changeBackground() {
-    console.log("Butona Tıkladın ");
+
+    //RASTGELE BİR RENK
+
+   /* const rastgeleSayi = Math.floor(Math.random() * colors.lenght);// Math.random rastgele sayı seçer_ Math.floor(Math.random() rastgele tam sayı seçer
+    const secilenRenk = colors[rastgeleSayi];//rastgele renk seç
+    console.log(rastgeleSayi, secilenRenk);
+    body.style.backgroundColor = secilenRenk;*/
+
+    //sırayla arkaplan rengi seçmek için
+
+    console.log(sıra);
+    if(sıra == 4) sıra = 0;
+    const secilenRenk = colors[sıra];
+    sıra++;
+    body.style.backgroundColor = secilenRenk;
+
 }
