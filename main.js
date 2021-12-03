76978bf5f745fab2792001377d6e5e99773c1450
Zehra Keşkek <fzk1800@gmail.com>
2021-12-03 19:28:18 +0300

    git güncellendi

---
+++ main.js
@@ -0,0 +1,10 @@
+const body = document.querySelector("body");
+const button = document.querySelector("button");
+,const colors =( "black", "green" , "grey", "blue")
+
+button.addEventListener("click", changeBackground);
+
+
+function changeBackground() {
+    console.log("Butona Tıkladın ");
+}
